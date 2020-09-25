23d2d441d550849b2dd86d2c1b6ee327afd64e28
Manvisekhri08 <manvisekhri08@gmail.com>
2020-09-25 21:33:38 +0530

    Dynamic Pages with templating

--- web-servers/src/app.js
+++ web-servers/src/app.js
@@ -6,7 +6,44 @@ const app = express()
 
 const publicDirectory = path.join(__dirname, "../public");
 
+app.set('view engine', 'hbs')
+
 app.use(express.static(publicDirectory));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Manvi Sekhri'
+    })
+})
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Page",
+    name: "Manvi Sekhri",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    name: "Manvi Sekhri",
+  });
+});
+
+app.get("/weather", (req, res) => {
+    res.send([{
+        forecast: 'Cloudy'
+    }, {
+        location: 'Pasadena'
+    }]);                  // arry of objects
+});
+
+app.listen(3000, () => {
+    console.log('SErver is up on port 3000!')
+})
+
+
 
 // app.get('', (req, res) => {
 //     res.send('<h1>hello express!</h1>')  // html
@@ -22,15 +59,3 @@ app.use(express.static(publicDirectory));
 // app.get("about.html", (req, res) => {
 //   res.send("<h1>About</h1>");
 // });
-
-app.get("/weather", (req, res) => {
-    res.send([{
-        forecast: 'Cloudy'
-    }, {
-        location: 'Pasadena'
-    }]);                  // arry of objects
-});
-
-app.listen(3000, () => {
-    console.log('SErver is up on port 3000!')
-})
