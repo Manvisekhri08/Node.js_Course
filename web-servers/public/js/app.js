e5bfe2af31252e3d1cb5bc549276e8c6b6bc7320
Manvisekhri08 <manvisekhri08@gmail.com>
2020-09-28 21:08:26 +0530

    User Interface

--- web-servers/public/js/app.js
+++ web-servers/public/js/app.js
@@ -2,18 +2,27 @@ console.log('Client side server!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector("input")
+const messageOne = document.querySelector("#message-1")
+const messageTwo = document.querySelector("#message-2")
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
+
+    messageOne.textContent = "Loading..."
+    messageTwo.textContent = ""
+
+
     fetch("http://localhost:3000/weather?address="+ location).then((response) => {
       response.json().then((data) => {
         if (data.error) {
-          console.log(data.error);
+          messageOne.textContent = data.error
         } else {
-          console.log(data.location);
-          console.log(data.forecast);
+            messageOne.textContent = data.location
+            const jsondata = JSON.stringify(data.forecast)
+            messageTwo.textContent = jsondata.toString()
         }
       });
     });
