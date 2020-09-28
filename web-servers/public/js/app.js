8a6c41053788a4c1801d0144d8182367563ae9f9
Manvisekhri08 <manvisekhri08@gmail.com>
2020-09-28 19:45:13 +0530

    Search Form

--- web-servers/public/js/app.js
+++ web-servers/public/js/app.js
@@ -1,12 +1,21 @@
 console.log('Client side server!')
 
-fetch("http://localhost:3000/weather?address=boston").then((response) => {
-    response.json().then((data) => {
+const weatherForm = document.querySelector('form')
+const search = document.querySelector("input")
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    fetch("http://localhost:3000/weather?address="+ location).then((response) => {
+      response.json().then((data) => {
         if (data.error) {
-          console.log(data.error)
-      } else {
-            console.log(data.location)
-            console.log(data.forecast)
+          console.log(data.error);
+        } else {
+          console.log(data.location);
+          console.log(data.forecast);
         }
-  });
-});
+      });
+    });
+    
+})
