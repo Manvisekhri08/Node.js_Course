a50ed26f9459b8f4d3a7a91fd32ed290c3c60539
Manvisekhri08 <manvisekhri08@gmail.com>
2020-10-02 23:09:01 +0530

    Inserting data and connecting to MongoDB

---
+++ Task-Manager-App/mongodb.js
@@ -0,0 +1,21 @@
+// CRUD create, read, update, delete
+
+const mongodb = require('mongodb')
+
+const MongoClient = mongodb.MongoClient
+
+const connectionUrl = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client ) => {
+    if (error) {
+       return console.log('Unable to connect to the database!')
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Louis',
+        age: 28
+    })
+})
