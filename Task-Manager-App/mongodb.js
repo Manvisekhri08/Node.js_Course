bec7ff89d6dd21fb5a3ba32699655eec257a4b0d
Manvisekhri08 <manvisekhri08@gmail.com>
2020-10-03 22:22:38 +0530

    Inserting documents

--- Task-Manager-App/mongodb.js
+++ Task-Manager-App/mongodb.js
@@ -14,8 +14,65 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    db.collection('users').insertOne({
-        name: 'Louis',
-        age: 28
-    })
+    // db.collection('users').insertOne({
+    //     name: 'Louis',
+    //     age: 28
+    // }, (error, result) => {
+    //         if (error) {
+    //             return console.log('Unable to insert data')
+    //         }
+
+    //         console.log(result.ops)
+    // })
+
+    // db.collection("users").insertMany([
+    //   {
+    //     name: "Harry Styles",
+    //     age: 26,
+    //   },
+    //   {
+    //     name: "Zayn Malik",
+    //     age: 27,
+    //   },
+    //   {
+    //     name: "Louis Tomlinson",
+    //     age: 28,
+    //   },
+    //   {
+    //     name: "Liam Payne",
+    //     age: 27,
+    //   },
+    //   {
+    //     name: "Niall Horan",
+    //     age: 27,
+    //   }
+    // ], (error, result) => {
+    //         if (error) {
+    //             return console.log("Unable to insert data");
+    //         }
+    //         console.log(result.ops)
+    // });
+db.collection("tasks").insertMany(
+  [
+    {
+      description: "Grocery Shopping",
+      completed: true,
+    },
+    {
+      description: "Studying",
+      completed: false,
+    },
+    {
+      description: "Playing",
+      completed: true,
+    }
+  ],
+  (error, result) => {
+    if (error) {
+      return console.log("Unable to insert tasks");
+    }
+    console.log(result.ops);
+  }
+);
+    
 })
