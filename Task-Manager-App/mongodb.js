a4cd5720690a1a7702682639d526b67d4823138c
Manvisekhri08 <manvisekhri08@gmail.com>
2020-10-04 23:42:34 +0530

    Querying documents

--- Task-Manager-App/mongodb.js
+++ Task-Manager-App/mongodb.js
@@ -6,15 +6,16 @@
 
 // const ObjectID = mongodb.ObjectID
 
+// const id = new ObjectID()
+// console.log(id.id.length)
+// console.log(id.getTimestamp())
+// console.log(id.toHexString().length)
+
 const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id.id.length)
-// console.log(id.getTimestamp())
-console.log(id.toHexString().length)
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client ) => {
     if (error) {
@@ -23,66 +24,14 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: 
 
     const db = client.db(databaseName)
 
-    // db.collection('users').insertOne({
-    //     _id: id,
-    //     name: 'Larry',
-    //     age: 27
-    // }, (error, result) => {
-    //         if (error) {
-    //             return console.log('Unable to insert data')
-    //         }
-
-    //         console.log(result.ops)
-    // })
-
-    // db.collection("users").insertMany([
-    //   {
-    //     name: "Harry Styles",
-    //     age: 26,
-    //   },
-    //   {
-    //     name: "Zayn Malik",
-    //     age: 27,
-    //   },
-    //   {
-    //     name: "Louis Tomlinson",
-    //     age: 28,
-    //   },
-    //   {
-    //     name: "Liam Payne",
-    //     age: 27,
-    //   },
-    //   {
-    //     name: "Niall Horan",
-    //     age: 27,
-    //   }
-    // ], (error, result) => {
-    //         if (error) {
-    //             return console.log("Unable to insert data");
-    //         }
-    //         console.log(result.ops)
-    // });
-// db.collection("tasks").insertMany(
-//   [
-//     {
-//       description: "Grocery Shopping",
-//       completed: true,
-//     },
-//     {
-//       description: "Studying",
-//       completed: false,
-//     },
-//     {
-//       description: "Playing",
-//       completed: true,
-//     }
-//   ],
-//   (error, result) => {
-//     if (error) {
-//       return console.log("Unable to insert tasks");
-//     }
-//     console.log(result.ops);
-//   }
-// );
+    db.collection('users').findOne({
+        name: 'Larry',
+        age: 26
+    }, (error, result) => {
+            if (error) {
+                return console.log('Unable to find the data')
+            }
+            console.log(result)
+    })
     
 })
