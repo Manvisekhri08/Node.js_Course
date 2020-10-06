d45a8ec9eb13c224b69a76c3c207d68469f551eb
Manvisekhri08 <manvisekhri08@gmail.com>
2020-10-06 16:16:12 +0530

    Querying documents

--- Task-Manager-App/mongodb.js
+++ Task-Manager-App/mongodb.js
@@ -18,20 +18,64 @@ const databaseName = 'task-manager'
 
 
 MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client ) => {
-    if (error) {
-       return console.log('Unable to connect to the database!')
-    }
+  if (error) {
+    return console.log("Unable to connect to the database!");
+  }
 
-    const db = client.db(databaseName)
+  const db = client.db(databaseName);
 
-    db.collection('users').findOne({
-        name: 'Larry',
-        age: 26
-    }, (error, result) => {
-            if (error) {
-                return console.log('Unable to find the data')
-            }
-            console.log(result)
+  // db.collection("users").findOne(
+  //   {
+  //     _id: new ObjectID("5f799bcf2664292bf04efa91"),
+  //   },
+  //   (error, result) => {
+  //     if (error) {
+  //       return console.log("Unable to find the data");
+  //     }
+  //     console.log(result);
+  //   }
+  // );
+  // db.collection("users").find(
+  //   {
+  //     age: 27
+  //   }).toArray((error, result) => {
+  //     if (error) {
+  //       return console.log("Unable to find the data");
+  //     }
+  //     console.log(result);
+  //   })
+
+  // db.collection("users")
+  //   .find({
+  //     age: 27,
+  //   })
+  //   .count((error, result) => {
+  //     if (error) {
+  //       return console.log("Unable to find the data");
+  //     }
+  //     console.log("Total count is "+result);
+  //   });
+
+//   db.collection("tasks").findOne(
+//     {
+//       _id: new ObjectID("5f78aba8a9735b144894228d"),
+//     },
+//     (error, result) => {
+//       if (error) {
+//         return console.log("Unable to find the data");
+//       }
+//       console.log(result);
+//     }
+//   );
+
+  db.collection("tasks")
+    .find({
+      completed: false,
     })
-    
+    .toArray((error, result) => {
+      if (error) {
+        return console.log("Unable to find the data");
+      }
+      console.log(result);
+    });
 })
